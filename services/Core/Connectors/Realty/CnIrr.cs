98cd4a5e965cafadf5cd371f6fb972b8a725ba7a
kuzminyv <kuzmin.spman@gmail.com>
2013-12-11 22:48:53 +0400

    Fix Irr connector

--- services/Core/Connectors/Realty/CnIrr.cs
+++ services/Core/Connectors/Realty/CnIrr.cs
@@ -9,6 +9,7 @@ using System.Text;
 using RGX = System.Text.RegularExpressions;
 using System.Threading;
 using Core.BLL;
+using Core.Expressions.AdParsers;
 
 namespace Core.Connectors
 {
@@ -24,15 +25,32 @@ namespace Core.Connectors
             get { return @"http://saratov.irr.ru/real-estate/apartments-sale/page{0}/"; }
         }
 
+        protected override IEnumerable<string> GetPageUrlFormats()
+        {
+            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BC%D0%B5%D0%BD%D1%8C%D1%88%D0%B5+1200000/currency=RUR/page{0}/";
+            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1200000+%D0%B4%D0%BE+1400000/currency=RUR/page{0}/";
+            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1400000+%D0%B4%D0%BE+1600000/currency=RUR/page{0}/";
+            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1600000+%D0%B4%D0%BE+1800000/currency=RUR/page{0}/";
+        }
+
+        public override int GetAvailablePagesCount(string pageFormat)
+        {
+            return 25;
+        }
+
+
         public override Selector CreateSelector()
         {
             return new BoundExpressionSelector("Ad", new BoundExpression(
-                    new BoundExpressionGroup("Start", new BoundSelector("add_list add_type4", ">")),
-                    new BoundExpressionGroup("Premium", true, new BoundSelector("add_premium", ">")),
-                    new BoundExpressionGroup("DetailUrl", new BoundSelector("add_list add_type4", "add_info", "a href=\"", "\"")),
-                    new BoundExpressionGroup("Description", new BoundSelector("\">", "</a>")),
-                    new BoundExpressionGroup("Price", new BoundSelector("add_cost", ">", "</div>")),
-                    new BoundExpressionGroup("Date", new BoundSelector("add_data",">", "</p>"))
+                    new BoundExpressionGroup("Start", new BoundSelector("add_type4", ">")),
+                    new BoundExpressionGroup("Date", true, new BoundSelector("adv_data", ">", "<")),
+                    new BoundExpressionGroup("DetailUrl", new BoundSelector("add_title_wrap", "href=\"", "\"")),
+                    new BoundExpressionGroup("Address", new BoundSelector("add_title", ">", "<")),
+                    new BoundExpressionGroup("Rooms", new BoundSelector("p_room", "<p>", "<")),
+                    new BoundExpressionGroup("LivingSpace", new BoundSelector("p_square", "<p>", "<")),
+                    new BoundExpressionGroup("Floor", new BoundSelector("p_etage", "<p>", "<")),
+                    new BoundExpressionGroup("Floors", new BoundSelector("sub>/", "<")),
+                    new BoundExpressionGroup("Price", new BoundSelector("add_cost", ">", "<"))
                 ));
         }
 
@@ -47,32 +65,6 @@ namespace Core.Connectors
 		    );            		
         }
 
-        protected override IEnumerable<string> GetPageUrlFormats()
-        {
-            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BC%D0%B5%D0%BD%D1%8C%D1%88%D0%B5+1200000/currency=RUR/page{0}/";
-            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1200000+%D0%B4%D0%BE+1400000/currency=RUR/page{0}/";
-            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1400000+%D0%B4%D0%BE+1600000/currency=RUR/page{0}/";
-            yield return "http://saratov.irr.ru/real-estate/apartments-sale/search/price=%D0%BE%D1%82+1600000+%D0%B4%D0%BE+1800000/currency=RUR/page{0}/";
-        }
-
-        public override int GetAvailablePagesCount(string pageFormat)
-        {
-            return 25;
-        }
-
-        private string[] _streetsDictionary;
-        public string[] StreetsDictionary
-        {
-            get
-            {
-                if (_streetsDictionary == null)
-                {
-                    _streetsDictionary = Managers.StreetsManager.GetList(8452/*Saratov city*/).SelectMany(s => s.Name.Split(' ').Where(n => n.Length > 3)).ToArray();
-                }
-                return _streetsDictionary;
-            }
-        }
-
         public override bool FillDetails(Ad ad)
         {
             return false;
@@ -82,88 +74,22 @@ namespace Core.Connectors
         {
             AdRealty ad = new AdRealty()
             {
-                Title = match["Description"],
-                Description = match["Description"],
-                Url = Id + match["DetailUrl"],
-                PublishDate = ParseDate(match["Date"]),
-                CreationDate = ParseDate(match["Date"]),
-                ConnectorId = this.Id,   
+                ConnectorId = this.Id,
                 BuildingType = Entities.Enums.BuildingType.Flat,
-                Price = ParsePrice(match["Price"])
+                Title = "",
+                Description = "",
+                Url = match["DetailUrl"],
+                Address = match["Address"],
+                RoomsCount = ParsersHelper.ParseInt(match["Rooms"]),
+                LivingSpace = ParsersHelper.ParseFloat(match["LivingSpace"]),
+                PublishDate = ParsersHelper.ParseDate(match["Date"], "HH:mm, dd.MM.yyyy"),
+                CreationDate = ParsersHelper.ParseDate(match["Date"], "HH:mm, dd.MM.yyyy"),
+                Price = ParsersHelper.ParseDouble(match["Price"]),
+                Floor = ParsersHelper.ParseInt(match["Floor"]),
+                FloorsCount = ParsersHelper.ParseInt(match["Floors"])
             };
 
-            string[] description = match["Description"].Split(',');
-
-
-            string roomsStr = description.FirstOrDefault(s => s.Contains("комн"));
-            if (roomsStr != null)
-            {
-                RGX.Regex rgx = new RGX.Regex(@"(?<Rooms>\d{1})");
-                var matches = rgx.Matches(roomsStr);
-                if (matches.Count == 1)
-                {
-                    ad.RoomsCount = int.Parse(matches[0].Groups["Rooms"].Value);
-                }
-            }
-
-
-            string spaceStr = description.FirstOrDefault(s => s.Contains("площадь") && s.Contains("общая"));
-            if (spaceStr != null)
-            {
-                RGX.Regex rgx = new RGX.Regex(@"(?<Space>\d{2,3})");
-                var matches = rgx.Matches(spaceStr);
-                if (matches.Count == 1)
-                {
-                    ad.LivingSpace = int.Parse(matches[0].Groups["Space"].Value);
-                }
-            }
-
-
-            string addressStr = description.FirstOrDefault(s =>
-                LastWith(s, "ул") ||
-                LastWith(s, "пр-кт") ||
-                LastWith(s, "проезд") ||
-                s.Contains("й проезд") ||
-                LastWith(s, "улица"));
-            addressStr = addressStr ?? description.FirstOrDefault(s => StreetsDictionary.Any(street => s.IndexOf(street, StringComparison.OrdinalIgnoreCase) > 0)); 
-            if (addressStr != null)
-            {
-                ad.Address = addressStr.Replace("адрес:", "");
-            }
-
-
-            RGX.Regex floorsRgx = new RGX.Regex(@"(?<Floors>\d{1,2}/\d{1,2})");
-            foreach (var descrPart in description)
-            {
-                var matches = floorsRgx.Matches(descrPart);
-                if (matches.Count == 1)
-                {
-                    var floors = matches[0].Groups["Floors"].Value.Split('/');
-                    ad.Floor = int.Parse(floors[0]);
-                    ad.FloorsCount = int.Parse(floors[1]);
-                }
-            }
             return ad;
-        }
-
-        private bool LastWith(string str, string subStr)
-        {
-            int idx = str.IndexOf(subStr, StringComparison.OrdinalIgnoreCase);
-            return idx > 0 && str.Length == (idx + subStr.Length);
-        }
-
-        protected double ParsePrice(string price)
-        {
-            if (price.Contains("руб"))
-            {
-                return double.Parse(price.Replace("руб.", "").Replace(".", "").Trim(), CultureInfo.InvariantCulture);
-            }
-            return 0;            
-        }
-
-        protected DateTime ParseDate(string date)
-        {
-            return DateTime.ParseExact(date.Trim(), "HH:mm, dd.MM.yyyy", CultureInfo.InvariantCulture);
         }
     }
 }
---
+++ services/Core/Expressions/AdParsers/ParsersHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RGX = System.Text.RegularExpressions;
+
+namespace Core.Expressions.AdParsers
+{
+    public class ParsersHelper
+    {
+        public static string RemoveAny(string str, params string[] strToRemove)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            foreach (var s in strToRemove)
+            {
+                str = str.Replace(s, "");
+            }
+            return str;
+        }
+
+        public static string NumbersOnly(string str, params char[] allowedChars)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsNumber(str[i]) || allowedChars.Any(c => c == str[i]))
+                {
+                    stringBuilder.Append(str[i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string RegexGroupValue(string str, string regex, string groupName)
+        {
+            RGX.Regex rgx = new RGX.Regex(regex);
+            var matches = rgx.Matches(str);
+            if (matches.Count == 1)
+            {
+                return matches[0].Groups[groupName].Value;
+            }
+            return string.Empty;
+        }
+
+        public static int ParseInt(string str, bool onlyDigits = true, params string[] strToRemove)
+        {
+            if (onlyDigits)
+            {
+                str = NumbersOnly(str);
+            }
+            if (strToRemove.Length > 0)
+            {
+                str = RemoveAny(str, strToRemove);
+            }
+            if (string.IsNullOrEmpty(str))
+            {
+                return default(int);
+            }
+            return int.Parse(str);
+        }
+
+        public static double ParseDouble(string str, bool onlyDigits = true, params string[] strToRemove)
+        { 
+            if (onlyDigits)
+            {
+                str = NumbersOnly(str);
+            }
+            if (strToRemove.Length > 0)
+            {
+                str = RemoveAny(str, strToRemove);
+            }
+            if (string.IsNullOrEmpty(str))
+            {
+                return default(double);
+            }
+            return double.Parse(str, CultureInfo.InvariantCulture);
+        }
+
+        public static float ParseFloat(string str, bool onlyDigits = true, params string[] strToRemove)
+        {
+            return (float)ParseDouble(str, onlyDigits, strToRemove);
+        }
+
+        public static DateTime ParseDate(string str, string format)
+        {
+            return DateTime.ParseExact(str.Trim(), format, CultureInfo.InvariantCulture);
+        }
+    }
+}
