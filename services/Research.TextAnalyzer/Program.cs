7048f6f7ebf1df35a92b8248d7eebee39f1da41c
kuzminyv <kuzmin.spman@gmail.com>
2014-04-23 13:02:51 +0400

    Small TextAnalyzer improvements.

--- services/Research.TextAnalyzer/Program.cs
+++ services/Research.TextAnalyzer/Program.cs
@@ -14,24 +14,40 @@ namespace Research.TextAnalyzer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Reading...");
             Query query = new Query(0, 10000);
             var result = Repositories.AdsRepository.GetList(query);
-            var uniqueWords = new HashSet<string>();
+            var uniqueWords = new Dictionary<string, int>();
 
+            Console.WriteLine("Analyzing...");
             foreach (var ad in result.Items)
             {
                 AdRealty adRealty = (AdRealty)ad;
-                uniqueWords.UnionWith(GetWordAfter(ad.Description, "ЖК"));
+                var words = GetWordsAfter(ad.Description, "ЖСК");
+                foreach (var word in words)
+                {
+                    var upperWord = word.ToUpper();
+                    if (uniqueWords.ContainsKey(upperWord))
+                    {
+                        uniqueWords[upperWord]++;
+                    }
+                    else
+                    {
+                        uniqueWords.Add(upperWord, 1);
+                    }
+                }
             }
 
-            foreach (var word in uniqueWords)
+            foreach (var word in uniqueWords.OrderByDescending(kvp => kvp.Value))
             {
-                Console.WriteLine(word);
+                Console.WriteLine("{0} - {1}", word.Value, word.Key);
             }
+
+            Console.WriteLine("Done.");
             Console.ReadKey();
         }
 
-        public static string[] GetWordAfter(string text, string wordBefore)
+        public static string[] GetWordsAfter(string text, string wordBefore)
         {
             var words = TextHelper.GetWords(text);
             var result = new List<string>();
--- services/Research.TextAnalyzer/TextHelper.cs
+++ services/Research.TextAnalyzer/TextHelper.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Research.TextAnalyzer
 {
     public class TextHelper
     {
-        public static string[] GetWords(string text)
+        public static string[] GetWords(string input)
         {
-            if (string.IsNullOrWhiteSpace(text))
+            if (string.IsNullOrWhiteSpace(input))
             {
                 return new string[0];
             }
 
-            return text.Split(new char[] { ' ', '.', ',', '\"', '\'' }, StringSplitOptions.RemoveEmptyEntries).Where(w => w.Length > 1).ToArray();
+            MatchCollection matches = Regex.Matches(input, @"\b[\w']*\b");
+
+            var words = from m in matches.Cast<Match>()
+                        where !string.IsNullOrEmpty(m.Value)
+                        select TrimSuffix(m.Value);
+
+            return words.ToArray();
+        }
+
+        static string TrimSuffix(string word)
+        {
+            int apostropheLocation = word.IndexOf('\'');
+            if (apostropheLocation != -1)
+            {
+                word = word.Substring(0, apostropheLocation);
+            }
+
+            return word;
         }
     }
 }
